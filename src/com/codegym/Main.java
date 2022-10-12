ef53bbc7d495443d625e2fdfb48eac35c088565e
binh113 <quocbinh230597@gmail.com>
2022-10-12 14:56:01 +0700

    first commit

---
+++ src/com/codegym/Main.java
@@ -0,0 +1,9 @@
+package com.codegym;
+
+public class Main {
+    public static void main(String[] args) {
+        Circle circle = new Circle(8);
+        System.out.println(circle.getArea());
+        System.out.println(circle.getRadius());
+    }
+}
